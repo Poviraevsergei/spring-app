f11ceda1bfc4ad1c528effef469701cc57b9e22e
poviraevsergei <Siarhey.Pavirayeu@ventionteams.com>
2025-10-30 19:22:07 +0300

    Interceptor

---
+++ src/main/java/by/tms/interceptor/LogInterceptor.java
@@ -0,0 +1,26 @@
+package by.tms.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class LogInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("Before controller: " + request.getRequestURI());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("After controller: " + request.getRequestURI());
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("After completion: " + request.getRequestURI());
+    }
+}
